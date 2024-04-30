59f99b6d5ec2db8823814ae81589fc4404a43880
BenieDIA <benieoyoukou223@icloud.com>
2024-04-30 12:47:01 +0200

    second commit

---
+++ js/app.js
@@ -0,0 +1,48 @@
+
+
+(()=>{
+console.log("hello")
+const version = 8.2
+let nom = "Javascript"
+console.log(version)
+const x = null
+let phone =20.00
+const lang =["html","css","js", true]
+console.table(lang)
+const user = {
+    nom :"Jake",
+    prenom : "Griffin",
+    age : 30,
+    ville : "NY",
+    pays : "USA"
+    
+    }
+
+    console.log(user)
+ console.table(user)
+console.log(`${user.nom} : ${user.prenom}`)
+console.log(lang.length) 
+/*for(let i=0; i < lang.length i++){
+     console.log(lang[i])
+}*/
+/*let i=0;
+while(i < lang.length){console.log(`index ${i} : $(lang{i})  `)
+
+
+*/
+lang.forEach(element =>{
+    console.log(Object.values(user))
+    
+
+
+});
+
+}
+
+
+
+
+
+
+ 
+)()
